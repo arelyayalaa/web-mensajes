daf1a52f01df0739a04831596b7d8963d19eda9b
arelyayalaa <110944026+arelyayalaa@users.noreply.github.com>
2024-04-06 18:22:11 -0600

    Add files via upload

---
+++ seis/script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Fecha de inicio de la relación (pon aquí la fecha de inicio)
+    const fechaInicio = new Date('2023-10-05');
+    
+    fetch('mensajes.json')
+    .then(response => response.json())
+    .then(data => {
+        const fechaActual = new Date();
+        
+        // Calculamos la diferencia de días entre la fecha actual y la fecha de inicio
+        const diferenciaTiempo = fechaActual.getTime() - fechaInicio.getTime();
+        const diferenciaDias = Math.floor(diferenciaTiempo / (1000 * 3600 * 24));
+
+        // Mensaje de aniversario
+        const mensajeAniversario = `Y oye, feliz aniversario de ${diferenciaDias -1} días:)`;
+
+        // Buscamos el mensaje correspondiente al día actual en el JSON
+        const dia = String(fechaActual.getDate()).padStart(2, '0');
+        const mes = String(fechaActual.getMonth() + 1).padStart(2, '0');
+        const claveFecha = `${mes}-${dia}`; // Formateamos la fecha
+        const mensajeCumple = data[claveFecha] || "Te amo, mi bonita.";
+
+        // Mostramos el mensaje de aniversario y el mensaje del día en la página
+        const mensajeDiv = document.getElementById("mensaje");
+        mensajeDiv.innerHTML = `<p>${mensajeCumple}</p><p>${mensajeAniversario}</p>`;
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo JSON:', error);
+    });
+});
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     fetch('mensajes.json')
+//         .then(response => response.json())
+//         .then(data => {
+//             const fechaActual = new Date();
+//             const dia = String(fechaActual.getDate()).padStart(2, '0');
+//             const mes = String(fechaActual.getMonth() + 1).padStart(2, '0');
+//             const claveFecha = `${mes}-${dia}`; // Formateamos la fecha
+
+//             const mensaje = data[claveFecha] || "Te amo, bonita.";
+
+//             const mensajeDiv = document.getElementById("mensaje");
+//             mensajeDiv.textContent = mensaje;
+//         })
+//         .catch(error => {
+//             console.error('Error al cargar el archivo JSON:', error);
+//         });
+// });
